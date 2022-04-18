b183a65ab0e7aa7f7bb4f9139f22fb9a304e121d
AlexanderShatalov <abshatalov87@gmail.com>
2022-04-18 18:33:03 +0300

    PUSH and GET half done add fix

--- src/test/java/userTests/User_Test.java
+++ src/test/java/userTests/User_Test.java
@@ -14,19 +14,25 @@ import static services.user.UserApi.BASE_URL;
 public class User_Test {
   UserApi userApi = new UserApi();
 
-  @Test //TODO дописать кейсы
+  //Проверить, что при создании Юзера в поле message записывается id нового пользователя
+  //Проверить, что при создании Юзера в поле type записывается значение "unknown"
+  @Test
   public void checkCreateUser() {
     Specifications.installSpecification(Specifications.requestSpec(BASE_URL), Specifications.responseSpecSchemaPlus200("schema/user/CreateUser.json"));
     User user = User.builder()
         .firstName("Ivan")
         .lastName("Ivanovich")
-        .username("Ivan1234")
+        .username("Ivan" + (int) (Math.random() * 100000))
         .password("123")
         .id((int) (Math.random() * 100000))
-        .userStatus(3)
+        .userStatus((int) (Math.random() * 10))
         .email("example" + ((int) (Math.random() * 100000)) + "@ya.ru")
         .phone("+79" + ((int) (Math.random() * 999999999)))
         .build();
+
+    userApi.createUser(user).
+        then().
+        log().all();
 
 
     Response response = userApi.createUser(user);
@@ -40,21 +46,21 @@ public class User_Test {
 
   //Проверить, что домен почты у пользователя "@ya.ru"
   //Проверить, что телефон у клиента начинается с "+79"
+  //Проверить, что пароль не пустой
   @Test
   public void checkGetUser() {
     Specifications.installSpecification(Specifications.requestSpec(BASE_URL), Specifications.responseSpecSchemaPlus200("schema/user/GetUser.json"));
     User user = User.builder()
         .firstName("Ivan")
         .lastName("Ivanovich")
-        .username("Ivan" + (int) (Math.random() * 100000))
-        .password("123")
-        .id((int) (Math.random() * 100000))
-        .userStatus((int) (Math.random() * 10))
-        .email("example" + ((int) (Math.random() * 100000)) + "@ya.ru")
-        .phone("+79" + ((int) (Math.random() * 999999999)))
+        .username("Ivan1234")
+        .password("1234")
+        .id(44863)
+        .userStatus(3)
+        .email("example95046@ya.ru")
+        .phone("+79301543521")
         .build();
 
-    userApi.createUser(user);
     userApi.getUser(user.getUsername())
         .then()
         .log().all();
@@ -67,6 +73,7 @@ public class User_Test {
     Assert.assertEquals(phone, user.getPhone());
     Assert.assertTrue(user.getEmail().endsWith("@ya.ru"));
     Assert.assertTrue(user.getPhone().startsWith("+79"));
+    Assert.assertNotNull(user.getPassword());
   }
 }
 
